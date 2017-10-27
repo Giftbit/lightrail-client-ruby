d59180d7a4b9935ff6deec436ee10af7abb2933d
Tana Jukes <tana.j@lightrail.com>
2017-10-27 14:25:46 -0700

    Extract transaction methods from Connection class to Transaction class

--- lib/lightrail_client/connection.rb
+++ lib/lightrail_client/connection.rb
@@ -1,64 +1,5 @@
 module Lightrail
   class Connection
-
-    def self.code_drawdown(charge_params)
-      self.post_transaction(charge_params)
-    end
-
-    def self.code_pending(charge_params)
-      self.post_transaction(charge_params)
-    end
-
-    def self.card_id_drawdown(charge_params)
-      self.post_transaction(charge_params)
-    end
-
-    def self.card_id_pending(charge_params)
-      self.post_transaction(charge_params)
-    end
-
-    def self.card_id_fund(fund_params)
-      self.post_transaction(fund_params)
-    end
-
-    def self.void(transaction_params)
-      self.act_on_transaction(transaction_params, :void)
-    end
-
-    def self.capture(transaction_params)
-      self.act_on_transaction(transaction_params, :capture)
-    end
-
-    def self.refund(transaction_params)
-      self.act_on_transaction(transaction_params, :refund)
-    end
-
-    def self.post_transaction(transaction_params)
-      response = {}
-      if (transaction_params[:code])
-        code = transaction_params.delete(:code)
-        response = self.send :make_post_request_and_parse_response, "codes/#{code}/transactions", transaction_params
-      elsif (transaction_params[:cardId])
-        card_id = transaction_params.delete(:cardId)
-        response = self.send :make_post_request_and_parse_response, "cards/#{card_id}/transactions", transaction_params
-      else
-        raise Lightrail::LightrailArgumentError.new("Lightrail code or cardId required to post a transaction: #{transaction_params.inspect}")
-      end
-      response['transaction']
-    end
-
-    def self.act_on_transaction(transaction_params, action)
-      response = {}
-      if (transaction_params[:cardId])
-        card_id = transaction_params.delete(:cardId)
-        transaction_id = transaction_params.delete(:transactionId)
-        response = self.send :make_post_request_and_parse_response, "cards/#{card_id}/transactions/#{transaction_id}/#{action}", transaction_params
-      else
-        raise Lightrail::LightrailArgumentError.new("Lightrail cardId required to act on an existing transaction: #{transaction_params.inspect}")
-      end
-      response['transaction']
-    end
-
     def self.get_balance_details(by_code_or_card, fullcode_or_card_id)
       response = by_code_or_card == :code ?
           self.get_code_balance(fullcode_or_card_id) :
@@ -91,14 +32,6 @@ module Lightrail
 
     def self.get_card_id_balance(card_id)
       self.send :make_get_request_and_parse_response, "cards/#{card_id}/balance"
-    end
-
-    def self.handle_pending(card_id, transaction_id, void_or_capture, request_body)
-      self.send :make_post_request_and_parse_response, "cards/#{card_id}/transactions/#{transaction_id}/#{void_or_capture}", request_body
-    end
-
-    def self.post_refund(card_id, transaction_id, request_body)
-      self.send :make_post_request_and_parse_response, "cards/#{card_id}/transactions/#{transaction_id}/refund", request_body
     end
 
 
