df142b14469a61fa559a37e6a49a3a9e03bff04e
Tana Jukes <tana.j@lightrail.com>
2018-01-10 11:31:35 -0800

    Allow generation of unauthenticated shopperTokens with shopperId=''
    Includes minor version bump

--- lib/lightrail_client/shopper_token_factory.rb
+++ lib/lightrail_client/shopper_token_factory.rb
@@ -4,12 +4,12 @@ module Lightrail
       raise Lightrail::BadParameterError.new("Lightrail::api_key is not set") unless Lightrail::api_key
       raise Lightrail::BadParameterError.new("Lightrail::shared_secret is not set") unless Lightrail::shared_secret
 
-      raise Lightrail::BadParameterError.new("Must provide a contact with one of shopper_id, contact_id or user_supplied_id to generate a shopper token") unless (Lightrail::Validator.has_valid_shopper_id?(contact) ||
+      raise Lightrail::BadParameterError.new("Must provide a contact with one of shopper_id, contact_id or user_supplied_id to generate a shopper token") unless (Lightrail::Validator.has_valid_or_empty_shopper_id?(contact) ||
           Lightrail::Validator.has_valid_contact_id?(contact) ||
           Lightrail::Validator.has_valid_user_supplied_id?(contact))
 
       g = {}
-      if (Lightrail::Validator.has_valid_shopper_id?(contact))
+      if (Lightrail::Validator.has_valid_or_empty_shopper_id?(contact))
         g['shi'] = Lightrail::Validator.get_shopper_id(contact)
       elsif (Lightrail::Validator.has_valid_contact_id?(contact))
         g['coi'] = Lightrail::Validator.get_contact_id(contact)
