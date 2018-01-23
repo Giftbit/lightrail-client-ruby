07af2b30657bbcad5497bca817c379c120fd2856
Tana Jukes <tana.j@lightrail.com>
2018-01-23 10:30:46 -0800

    Handle error/regular cases with the same step definition
    Updated regex on feature matcher:
    - Capture groups exclude the closing square bracket: [^\]]
    - End section about throwing an error is optional: (?: and throws the following error: \[(.[^\]]+)\])?
    
    Removed original step definition as it became redundant.
    
    Also removed initial reading in of variables.json as it isn't available to function bodies as it was.

--- features/step_definitions/account_stepdefs.rb
+++ features/step_definitions/account_stepdefs.rb
@@ -1,21 +1,4 @@
-@variables = JSON.parse(File.open("features/variables.json").read)
-
-When(/^a contact .*\s*exists?\s*.*: requires minimum parameters \[(.+)\] and makes the following REST requests: \[(.+)\]$/) do |minimum_params, expectedRequestsAndResponses|
-  @variables = JSON.parse(File.open("features/variables.json").read)
-  min_param_keys = minimum_params.split(', ')
-  min_param_values = min_param_keys.map {|key| @variables[key]}
-  min_params = [min_param_keys, min_param_values].transpose.to_h
-
-  reqResCollection = expectedRequestsAndResponses.split(', ').map {|key| @variables['requestResponseCombos'][key]}
-
-  reqResCollection.each do |reqRes|
-    expect_to_call_endpoint_and_receive_result(reqRes['httpMethod'], reqRes['endpoint'], reqRes['response'])
-  end
-
-  Lightrail::Account.create(min_params)
-end
-
-When(/^a contact .*\s*exists?\s*.*: requires minimum parameters \[(.+)\] and makes the following REST requests: \[(.+)\] and throws the following error: \[(.+)\]$/) do |minimum_params, expectedRequestsAndResponses, errorName|
+When(/^a contact .*\s*exists?\s*.*: requires minimum parameters \[(.[^\]]+)\] and makes the following REST requests: \[(.[^\]]+)\](?: and throws the following error: \[(.[^\]]+)\])?$/) do |minimum_params, expectedRequestsAndResponses, errorName|
   @variables = JSON.parse(File.open("features/variables.json").read)
   min_param_keys = minimum_params.split(', ')
   min_param_values = min_param_keys.map {|key| @variables[key]}
@@ -33,7 +16,11 @@ When(/^a contact .*\s*exists?\s*.*: requires minimum parameters \[(.+)\] and mak
     end
   end
 
-  expect {Lightrail::Account.create(min_params)}.to raise_error(Lightrail.const_get(errorName))
+  if errorName
+    expect {Lightrail::Account.create(min_params)}.to raise_error(Lightrail.const_get(errorName))
+  else
+    Lightrail::Account.create(min_params)
+  end
 end
 
 
