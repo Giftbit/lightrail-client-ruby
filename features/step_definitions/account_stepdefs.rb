3b4ab0ab2ee2d55bcb1483b78474fe045873d7ac
Tana Jukes <tana.j@lightrail.com>
2018-01-23 14:34:04 -0800

    Add reminder notes (documentation)

--- features/step_definitions/account_stepdefs.rb
+++ features/step_definitions/account_stepdefs.rb
@@ -1,5 +1,9 @@
 When(/^a contact .*\s*exists?\s*.*: requires minimum parameters \[(.[^\]]+)\] and makes the following REST requests: \[(.[^\]]+)\](?: and throws the following error: \[(.[^\]]+)\])?$/) do |minimum_params, expectedRequestsAndResponses, errorName|
+
+  # Consider reading this in outside of this function if more than one stepdef in this file needs it
+  # Also consider breaking variables.json into feature-specific files (account_feature_variables.json, contact_feature_variables.json, etc)
   @variables = JSON.parse(File.open("features/variables.json").read)
+
   min_param_keys = minimum_params.split(', ')
   min_param_values = min_param_keys.map {|key| @variables[key]}
   min_params = [min_param_keys, min_param_values].transpose.to_h
